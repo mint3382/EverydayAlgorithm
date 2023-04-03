fc6d5ac8dab1652a686ea8d3582efa5569d0c789
mint3382 <mskim0926@naver.com>
2023-04-03 20:06:29 +0900

    배열_알파벳개수

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -5,4 +5,17 @@
 //  Created by minsong kim on 12/23/23.
 //
 
-import Foundation
+//배열
+//알파벳 개수
+//let S = readLine()!
+//let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+//
+//for i in alphabet {
+//    var count = 0
+//    for j in S {
+//        if i == String(j) {
+//            count += 1
+//        }
+//    }
+//    print(count, terminator: " ")
+//}
