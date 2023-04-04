ad0321d8f02b565d012ca56225ba256a7d82f409
mint3382 <mskim0926@naver.com>
2023-04-04 20:09:51 +0900

    배열_방번호

---
+++ BaekJoon/BaekJoon/브론즈/배열.swift
@@ -0,0 +1,34 @@
+//
+//  배열.swift
+//  BaekJoon
+//
+//  Created by minsong kim on 12/23/23.
+//
+
+//배열
+//숫자의 개수
+//import Foundation
+//let a = readLine()!
+//let b = readLine()!
+//let c = readLine()!
+//
+//let x = (Int(a)! * Int(b)! * Int(c)!)
+//
+//for i in 0...9 {
+//    print(String(x).components(separatedBy: "\(i)").count - 1)
+//}
+
+//알파벳 개수
+//let S = readLine()!
+//let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+//
+//for i in alphabet {
+//    var count = 0
+//    for j in S {
+//        if i == String(j) {
+//            count += 1
+//        }
+//    }
+//    print(count, terminator: " ")
+//}
+
---
+++ BaekJoon/BaekJoon/실버/배열(실버).swift
@@ -0,0 +1,20 @@
+//
+//  배열(실버).swift
+//  BaekJoon
+//
+//  Created by minsong kim on 12/23/23.
+//
+
+//방 번호
+//let N = readLine()!
+//var arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+//
+//for i in N {
+//    arr[Int(String(i))!] += 1
+//}
+//
+//arr[6] = (arr[6] + arr[9] + 1) / 2
+//arr[9] = 0
+//
+//print(arr.max()!)
+
