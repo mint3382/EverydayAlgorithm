0e5028f3fe57e58162d6282107104d4dcbc726f2
mint3382 <mskim0926@naver.com>
2023-04-05 20:11:12 +0900

    배열_두 수의 합

--- BaekJoon/BaekJoon/실버/배열(실버).swift
+++ BaekJoon/BaekJoon/실버/배열(실버).swift
@@ -18,3 +18,30 @@
 //
 //print(arr.max()!)
 
+//두 수의 합
+//import Foundation
+//let n = readLine()!
+//var arr = readLine()!.split(separator: " ").sorted()
+//let x = Int(readLine()!)!
+//var count = 0
+//var arr2 = [Int](repeating: 0, count: x - 1)
+//
+//for i in arr {
+//    if Int(i)! < x {
+//        arr2[Int(i)! - 1] += 1
+//    }
+//}
+//
+//if x == 1 || x == 2 {
+//    print(count)
+//} else {
+//    for i in 0...((x - 1) / 2) - 1 {
+//        if arr2[i] == 1 && arr2[i] == arr2[x - i - 2] {
+//            count += 1
+//        }
+//    }
+//}
+//
+//print(count)
+//
+//
