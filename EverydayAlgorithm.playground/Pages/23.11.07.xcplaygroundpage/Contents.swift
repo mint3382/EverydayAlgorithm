ec388a86b71509d58e180de7c8ab7935fd80764a
mint3382 <mskim0926@naver.com>
2023-11-07 18:55:39 +0900

    feat: 백준 두수의 합

---
+++ EverydayAlgorithm.playground/Pages/23.11.07.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//두수의 합
+let n = readLine()!
+var arr = readLine()!.split(separator: " ").sorted()
+let x = Int(readLine()!)!
+var count = 0
+var arr2 = [Int](repeating: 0, count: x - 1)
+
+for i in arr {
+    if Int(i)! < x {
+        arr2[Int(i)! - 1] += 1
+    }
+}
+
+if x == 1 || x == 2 {
+    print(count)
+} else {
+    for i in 0...((x - 1) / 2) - 1 {
+        if arr2[i] == 1 && arr2[i] == arr2[x - i - 2] {
+            count += 1
+        }
+    }
+}
+
+print(count)
+
+
+//: [Next](@next)
