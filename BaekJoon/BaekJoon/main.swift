dcecd99feb57766db27b03326cd515e468f6add5
mint3382 <mskim0926@naver.com>
2023-12-22 19:54:41 +0900

    알고리즘

---
+++ BaekJoon/BaekJoon/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  BaekJoon
+//
+//  Created by minsong kim on 12/23/23.
+//
+
+import Foundation
+
---
+++ BaekJoon/BaekJoon/대회/첫번째 나들이.swift
@@ -0,0 +1,91 @@
+//
+//  첫번째 나들이.swift
+//  BaekJoon
+//
+//  Created by minsong kim on 12/23/23.
+//
+
+//선택의 기로
+//let n = Int(readLine()!)!
+//var q = [Int]()
+//var p = [Int]()
+//
+//for _ in 1...n {
+//    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+//    q.append(input[0])
+//    p.append(input[1])
+//}
+//
+//func first(_ q: [Int], _ p: [Int]) -> Int {
+//    var max = 0
+//    var index = 0
+//
+//    for i in 0...q.count - 1 {
+//        if q[i] > max {
+//            max = q[i]
+//            index = i
+//        } else if q[i] == max {
+//            if p[i] <= p[index] {
+//                max = q[i]
+//                index = i
+//            }
+//        }
+//    }
+//
+//    return index
+//}
+//
+//func second(_ q: [Int], _ p: [Int]) -> Int {
+//    var min = 10000
+//    var index = 0
+//
+//    for i in 0...p.count - 1 {
+//        if p[i] < min {
+//            min = p[i]
+//            index = i
+//        } else if p[i] == min {
+//            if q[i] >= q[index] {
+//                min = p[i]
+//                index = i
+//            }
+//        }
+//    }
+//
+//    return index
+//}
+//
+//func firstAnswer(q: [Int], p: [Int]) -> String {
+//    var answer = ""
+//    var quality = q
+//    var price = p
+//    let firstIndex = first(q, p)
+//
+//    answer += "\(q[firstIndex]) \(p[firstIndex]) "
+//    price.remove(at: firstIndex)
+//    quality.remove(at: firstIndex)
+//
+//    let secondIndex = first(quality, price)
+//    answer += "\(quality[secondIndex]) \(price[secondIndex])"
+//
+//    return answer
+//}
+//
+//func secondAnswer(q: [Int], p: [Int]) -> String {
+//    var answer = ""
+//    var quality = q
+//    var price = p
+//    let firstIndex = second(q, p)
+//
+//    answer += "\(q[firstIndex]) \(p[firstIndex]) "
+//    price.remove(at: firstIndex)
+//    quality.remove(at: firstIndex)
+//
+//    let secondIndex = second(quality, price)
+//    answer += "\(quality[secondIndex]) \(price[secondIndex])"
+//
+//    return answer
+//}
+//
+//print(firstAnswer(q: q, p: p))
+//print(secondAnswer(q: q, p: p))
+//
